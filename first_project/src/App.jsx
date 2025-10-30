c77f4324f8aac15fe7c305bcd08127fefb10d67f
Ayush Tomar <ayushsinghtomar184@gmail.com>
2025-10-30 15:38:27 +0530

    first react project

---
+++ first_project/src/App.jsx
@@ -0,0 +1,15 @@
+import Car from './Vehicle'
+import './App.css'
+function App() {
+
+  return (
+    <>
+     <Car/>
+     <h1>testing component behavior</h1>
+     <h2>Testing</h2>
+     <Car/>
+    </>
+  )
+}
+
+export default App
---
+++ first_project/src/Vehicle.jsx
@@ -0,0 +1,11 @@
+function Car(){
+  return(
+    <>
+    <h1>THIS IS COMPONENT EXAMPLE</h1>
+    <button>First</button>
+    <button>Second</button>
+    <button>Third</button>
+    </>
+  )
+}
+export default Car;
