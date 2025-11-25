30aaf48e536a87142f89bb3ff5ab9d0c84b73103
Ayush Tomar <ayushsinghtomar184@gmail.com>
2025-11-25 08:58:45 +0530

    update the script

--- 11sept.js
+++ 11sept.js
@@ -49,4 +49,3 @@ document.getElementById("message").style.cursor = "pointer"; // Change cursor on
 document.getElementById("message").style.userSelect = "none"; // Disable text selection
 document.getElementById("message").style.overflow = "hidden"; // Hide overflow
 document.getElementById("message").style.textOverflow = "ellipsis"; // Ellipsis for overflow text
-document.getElementById("message").style.whiteSpace = "nowrap"; // Prevent text wrapping
